d1fe73cc5d89661cd5ddb551a10068536f405178
Vincent Sisk <vinnysisk@gmail.com>
2015-05-11 19:03:59 -0600

    add edit restaurant name functionality

--- vagrant/webserver.py
+++ vagrant/webserver.py
@@ -32,7 +32,7 @@ class webServerHandler(BaseHTTPRequestHandler):
                     output += "<section><h3>"
                     output += "%s" % restaurant.name
                     output += "</h3>"
-                    output += "<p><a href='#'>Edit</a></p>"
+                    output += "<p><a href='/restaurants/%s/edit'>Edit</a></p>" % restaurant.id
                     output += "<p><a href='#'>Delete</a></p>"
                     output += "</section>"
 
@@ -49,7 +49,6 @@ class webServerHandler(BaseHTTPRequestHandler):
                 output = ""
                 output += "<html><body>"
 
-                # start coding here
                 output += "<h1>Make a New Restaurant</h1>"
                 output += "<form method='POST' enctype='multipart/form-data' action='/restaurants/new'>"
                 output += "<input name='newRestaurantName' type='text' placeholder='New Restaurant Name' />"
@@ -57,6 +56,29 @@ class webServerHandler(BaseHTTPRequestHandler):
 
                 output += "</body></html>"
                 self.wfile.write(output)
+                print output
+                return
+
+            if self.path.endswith("/edit"):
+                restaurantIDPath = self.path.split("/")[2]
+                myRestaurantQuery = session.query(Restaurant).filter_by(id = restaurantIDPath).one()
+
+                if myRestaurantQuery != []:
+                    self.send_response(200)
+                    self.send_header('Content-type', 'text/html')
+                    self.end_headers()
+
+                
+                    output = ""
+                    output += "<html><body>"
+
+                    output += "<h1>%s</h1>" % myRestaurantQuery.name
+                    output += "<form method='POST' enctype='multipart/form-data' action='/restaurants/%s/edit'>" % restaurantIDPath
+                    output += "<input name='newRestaurantName' type='text' placeholder='%s' />" % myRestaurantQuery.name
+                    output += "<input type='submit' value='Rename' /></form>"
+                
+                    output += "</body></html>"
+                    self.wfile.write(output)
                 print output
                 return
 
@@ -100,6 +122,26 @@ class webServerHandler(BaseHTTPRequestHandler):
                 newRestaurant = Restaurant(name = messagecontent[0])
                 session.add(newRestaurant)
                 session.commit()
+                
+                self.send_response(301)
+                self.send_header('Content-type', 'text/html')
+                self.send_header('Location', '/restaurants')
+                self.end_headers()
+
+                return
+
+            if self.path.endswith("/edit"):
+                ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                messagecontent = fields.get('newRestaurantName')
+                restaurantIDPath = self.path.split("/")[2]
+
+                myRestaurantQuery = session.query(Restaurant).filter_by(id = restaurantIDPath).one()
+                if myRestaurantQuery != []:
+                    myRestaurantQuery.name = messagecontent[0]
+                    session.add(myRestaurantQuery)
+                    session.commit()
                 
                 self.send_response(301)
                 self.send_header('Content-type', 'text/html')
